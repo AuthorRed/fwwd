3bf5dfae29da83f4e3d16de85ac1f980bc310a92
xiefei <liang6.-0@163.com>
2021-01-08 14:28:04 +0800

    Initial commit

---
+++ src/main/java/cn/author/fwwd/FwwdApplication.java
@@ -0,0 +1,13 @@
+package cn.author.fwwd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FwwdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FwwdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/author/fwwd/FwwdApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.author.fwwd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FwwdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
