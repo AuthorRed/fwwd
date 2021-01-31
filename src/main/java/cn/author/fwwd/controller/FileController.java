93c0233daf0e890b8f1109a3b2dd2476e297d652
AuthorRed <liang6.-0@163.com>
2021-01-31 22:08:37 +0800

    添加代码

--- src/main/java/cn/author/fwwd/controller/FileController.java
+++ src/main/java/cn/author/fwwd/controller/FileController.java
@@ -4,6 +4,7 @@ import cn.author.fwwd.Utils.DateUtils;
 import cn.author.fwwd.Utils.FileContentTypeUtils;
 import cn.author.fwwd.common.ResultMsg;
 import cn.author.fwwd.config.PropertiesConfig;
+import cn.author.fwwd.dao.model.Attach;
 import cn.author.fwwd.enums.ServiceID;
 import cn.author.fwwd.service.FileService;
 import lombok.extern.slf4j.Slf4j;
@@ -48,34 +49,34 @@ public class FileController {
     }
     @GetMapping("getFileById")
     public void getFileById(Long id, HttpServletResponse response){
-        try {
-            Attach attach = fileService.selectByPrimaryKey(id);
-            if(null==attach){
-                return;
-            }
-            String fileName = attach.getFileName();
-            String extention = fileName.substring(fileName.lastIndexOf("."));
-            if(null==attach || StringUtils.isBlank(attach.getUrl())|| StringUtils.isBlank(fileName)|| StringUtils.isBlank(extention)){
-                return;
-            }
-            File file = new File(attach.getUrl());
+//        try {
+//            Attach attach = fileService.selectByPrimaryKey(id);
+//            if(null==attach){
+//                return;
+//            }
+//            String fileName = attach.getFileName();
+//            String extention = fileName.substring(fileName.lastIndexOf("."));
+//            if(null==attach || StringUtils.isBlank(attach.getUrl())|| StringUtils.isBlank(fileName)|| StringUtils.isBlank(extention)){
+//                return;
+//            }
+//            File file = new File(attach.getUrl());
 //            String l=request.getRealPath("/")+"/"+url;
 //            String filename = file.getName();
-            InputStream fis = new BufferedInputStream(new FileInputStream(file));
-            byte[] buffer = new byte[fis.available()];
-            fis.read(buffer);
-            fis.close();
-            response.reset();
-            // 设置response的Header
-            response.addHeader("Content-Length", "" + file.length());
-            response.setContentType(FileContentTypeUtils.getContentType(extention));
-
-            OutputStream toClient = new BufferedOutputStream(response.getOutputStream());
-            toClient.write(buffer);
-            toClient.flush();
-            toClient.close();
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
+//            InputStream fis = new BufferedInputStream(new FileInputStream(file));
+//            byte[] buffer = new byte[fis.available()];
+//            fis.read(buffer);
+//            fis.close();
+//            response.reset();
+//            // 设置response的Header
+//            response.addHeader("Content-Length", "" + file.length());
+//            response.setContentType(FileContentTypeUtils.getContentType(extention));
+//
+//            OutputStream toClient = new BufferedOutputStream(response.getOutputStream());
+//            toClient.write(buffer);
+//            toClient.flush();
+//            toClient.close();
+//        } catch (IOException ex) {
+//            ex.printStackTrace();
+//        }
     }
 }
